9e1d826aee18b5cb1723df4077f09b7acb90b90d
mgbttt <44745409+mgbttt@users.noreply.github.com>
2022-12-13 23:50:47 +0100

    Update cursed_secret_party_03.js

--- cursed_secret_party_03.js
+++ cursed_secret_party_03.js
@@ -1,11 +1,5 @@
 fetch('http://172.17.0.1:8086/prueba.txt', {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        "prueba"
-    })
+    method: 'GET'
 })
 .then((res) => res.text())
 .then((data) => {
